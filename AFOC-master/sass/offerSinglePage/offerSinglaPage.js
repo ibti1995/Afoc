8a4e62da1d9fe0eb663474a857d345428b883ff1
ibti1995 <ibtissembouchnak@gmail.com>
2020-10-22 22:14:40 +0200

    offers page

--- AFOC-master/sass/offerSinglePage/offerSinglaPage.js
+++ AFOC-master/sass/offerSinglePage/offerSinglaPage.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -23,4 +23,33 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
  // dots[slideIndex-1].class += " active";
+}
+/******************************************************************/ 
+
+
+
+
+var slideIndex = [1,1,1,1,1,1];
+var slideId = ["mySlides", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
 }
