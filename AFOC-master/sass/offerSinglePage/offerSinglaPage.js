d29122dd13577b21c52456784df3b75943844099
ibti1995 <ibtissembouchnak@gmail.com>
2020-10-23 12:56:11 +0200

    modification1

--- AFOC-master/sass/offerSinglePage/offerSinglaPage.js
+++ AFOC-master/sass/offerSinglePage/offerSinglaPage.js
@@ -37,19 +37,25 @@ showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
 showSlides(1, 5);
-showSlides(1, 6);
+
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
+/*
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}*/
 
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
+ // var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no]-1].style.display = "block";
+  //dots[slideIndex[no]-1].className += " active";
 }
