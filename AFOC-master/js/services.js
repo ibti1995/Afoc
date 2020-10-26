5da3ff5f68ff5ee5c484c6ad0f9682203f2a4ea4
TaibABROUG <taibabro@gmail.com>
2020-10-26 09:30:22 +0000

    wedding+ acrtivitie

---
+++ AFOC-master/js/services.js
@@ -0,0 +1,28 @@
+var slide4Index = 1;
+showSlides4(slide4Index);
+
+// Next/previous controls
+function plusSlides4(n) {
+  showSlides4(slide4Index += n);
+}
+
+// Thumbnail image controls
+function currentSlide4(n) {
+  showSlides4(slide4Index = n);
+}
+
+function showSlides4(n4) {
+  var j;
+  var slides4 = document.getElementsByClassName("mySlides4");
+  var dots = document.getElementsByClassName("dot4");
+  if (n4 > slides4.length) {slide4Index = 1}
+  if (n4 < 1) {slide4Index = slides4.length}
+  for (j = 0; j < slides4.length; j++) {
+      slides4[j].style.display = "none";
+  }
+  for (j = 0; j < dots.length; j++) {
+      dots[j].className = dots[j].className.replace(" active4", "");
+  }
+  slides4[slide4Index-1].style.display = "block";
+  dots[slide4Index-1].className += " active4";
+}
---
+++ AFOC-master/js/weddings-hall.js
@@ -0,0 +1,26 @@
+var slide5Index = 1;
+showSlides5(slide5Index);
+
+// Next/previous controls
+function plusSlides5(n) {
+  showSlides5(slide5Index += n);
+}
+
+// Thumbnail image controls
+function currentSlide5(n) {
+  showSlides5(slide5Index = n);
+}
+
+function showSlides5(n5) {
+  var j;
+  var slides5 = document.getElementsByClassName("mySlides5");
+
+  if (n5 > slides5.length) {slide5Index = 1}
+  if (n5 < 1) {slide5Index = slides5.length}
+  for (j = 0; j < slides5.length; j++) {
+      slides5[j].style.display = "none";
+  }
+  
+  slides5[slide5Index-1].style.display = "block";
+  
+}
