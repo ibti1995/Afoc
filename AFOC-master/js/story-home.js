9676cd45c1101e292d98dcbb5f7e5d8b96b072bd
TaibABROUG <taibabro@gmail.com>
2020-10-28 22:39:47 +0000

    home page 1000

--- AFOC-master/js/story-home.js
+++ AFOC-master/js/story-home.js
@@ -3,19 +3,22 @@ showSlidesac(2);
     var ji=0;
     var slidesac3 = document.getElementsByClassName("myslidestory");
     var dots = document.getElementsByClassName("demo4");
-  
-  
+    
+    var dots2 = document.getElementsByClassName("underline-home");
     for (ji = 0; ji < slidesac3.length; ji++) {
   
         slidesac3[ji].style.visibility= "hidden";
     //   slidesac3[ji].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active6", "");
+        dots[i].className = dots[i].className.replace(" active-stay-home", "");
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots2[i].style.visibility= "hidden";
       }
     slidesac3[n-1].style.visibility = "visible";
-    dots[n-1].className += " active6";
-    
+    dots[n-1].className += " active-stay-home";
+    dots2[n-1].style.visibility = "visible";
   
     
   }
