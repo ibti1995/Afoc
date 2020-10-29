1a712934d6ffcc1158ef52a07f1e048c0c81bfe5
ibti1995 <ibtissembouchnak@gmail.com>
2020-10-29 11:22:44 +0100

    push 15

--- AFOC-master/js/rooms.js
+++ AFOC-master/js/rooms.js
@@ -1,30 +1,55 @@
-var slideIndex = 1;
-showSlidesA(slideIndex);
+var slideIndex = [1,1,1,1,1,1,1,1,1];
+var slideId = ["slides1", "slides2","slides3","slides4","slides5","slides6","mySlides7", "mySlides8", "mySlides9"]
+showSlides6(1, 0);
+showSlides6(1, 1);
+showSlides6(1, 2);
+showSlides6(1, 3);
+showSlides6(1, 4);
+showSlides6(1, 5);
+showSlides6(1, 6);
+showSlides6(1, 7);
+showSlides6(1, 8);
 
-// Next/previous controls
-function plusSlidesA(n) {
-  showSlidesA(slideIndex += n);
+function plusSlides6(n, no) {
+    console.log(no , n );
+  showSlides6(slideIndex[no] += n, no);
 }
-
-// Thumbnail image controls
+/*
 function currentSlide(n) {
   showSlides(slideIndex = n);
-}
+}*/
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesA");
-
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function showSlides6(n, no) {
+  var i=0;
+  var x = document.getElementsByClassName(slideId[no]);
+ // var dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    console.log(x[i] ," length", x.length);
+    x[i].style.display = "none";
   }
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " activeA";
- 
-
-  
+  x[slideIndex[no]-1].style.display = "block";
+  //dots[slideIndex[no]-1].className += " active";
 }
+
+  // Thumbnail image controls
+showSlidesac(1);
+  function showSlidesac(n) {
+    var ji;
+    var slidesac = document.getElementsByClassName("mySlides3");
+    var dots = document.getElementsByClassName("demo3");
+  
+  
+    for (ji = 0; ji < slidesac.length; ji++) {
+      slidesac[ji].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active6", "");
+      }
+    slidesac[n-1].style.display = "block";
+    dots[n-1].className += " active6";
+    
+  
+    
+  }
