bc867fa81a6fd399df52a84fe1b500c6c5a9c63a
TaibABROUG <taibabro@gmail.com>
2020-11-13 20:56:03 +0000

    gallery slide

---
+++ AFOC-master/js/galerry-mob-for-item.js
@@ -0,0 +1,275 @@
+var slideIndexgallery = 1;
+showSlidesgallery(slideIndexgallery);
+
+
+// Next/previous controls
+function plusSlidesgallery(n) {
+  showSlidesgallery(slideIndexgallery += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery(n) {
+  showSlidesgallery(slideIndexgallery = n);
+}
+
+function showSlidesgallery(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery");
+ 
+  if (n > slides.length) {slideIndexgallery = 1}
+  if (n < 1) {slideIndexgallery = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery);
+   slideIntervalgallery = setInterval(changegallery,3000);
+      
+      });
+
+var slideIntervalgallery = setInterval(changegallery ,3000);
+
+
+    function changegallery() {
+       plusSlidesgallery(1);
+        
+    }
+
+//*************** G2 */
+var slideIndexgallery2 = 1;
+showSlidesgallery2(slideIndexgallery2);
+
+
+// Next/previous controls
+function plusSlidesgallery2(n) {
+  showSlidesgallery2(slideIndexgallery2 += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery2(n) {
+  showSlidesgallery2(slideIndexgallery2 = n);
+}
+
+function showSlidesgallery2(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery2");
+ 
+  if (n > slides.length) {slideIndexgallery2 = 1}
+  if (n < 1) {slideIndexgallery2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery2-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery2").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery2);
+   slideIntervalgallery2 = setInterval(changegallery2,3000);
+      
+      });
+
+var slideIntervalgallery2 = setInterval(changegallery2 ,3000);
+
+
+    function changegallery2() {
+       plusSlidesgallery2(1);
+        
+    }
+
+//************g3 */
+var slideIndexgallery3 = 1;
+showSlidesgallery3(slideIndexgallery3);
+
+
+// Next/previous controls
+function plusSlidesgallery3(n) {
+  showSlidesgallery3(slideIndexgallery3 += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery3(n) {
+  showSlidesgallery3(slideIndexgallery3 = n);
+}
+
+function showSlidesgallery3(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery3");
+ 
+  if (n > slides.length) {slideIndexgallery3 = 1}
+  if (n < 1) {slideIndexgallery3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery3-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery3").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery3);
+   slideIntervalgallery3 = setInterval(changegallery3,3000);
+      
+      });
+
+var slideIntervalgallery3 = setInterval(changegallery3 ,3000);
+
+
+    function changegallery3() {
+       plusSlidesgallery3(1);
+        
+    }
+
+
+    //***************G4 */
+    var slideIndexgallery4 = 1;
+showSlidesgallery4(slideIndexgallery4);
+
+
+// Next/previous controls
+function plusSlidesgallery4(n) {
+  showSlidesgallery4(slideIndexgallery4 += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery4(n) {
+  showSlidesgallery4(slideIndexgallery4 = n);
+}
+
+function showSlidesgallery4(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery4");
+ 
+  if (n > slides.length) {slideIndexgallery4 = 1}
+  if (n < 1) {slideIndexgallery4 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery4-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery4").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery4);
+   slideIntervalgallery4 = setInterval(changegallery4,3000);
+      
+      });
+
+var slideIntervalgallery4 = setInterval(changegallery4 ,3000);
+
+
+    function changegallery4() {
+       plusSlidesgallery4(1);
+        
+    }
+
+    //**************5 */
+    var slideIndexgallery5 = 1;
+showSlidesgallery5(slideIndexgallery5);
+
+
+// Next/previous controls
+function plusSlidesgallery5(n) {
+  showSlidesgallery5(slideIndexgallery5 += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery5(n) {
+  showSlidesgallery5(slideIndexgallery5 = n);
+}
+
+function showSlidesgallery5(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery5");
+ 
+  if (n > slides.length) {slideIndexgallery5 = 1}
+  if (n < 1) {slideIndexgallery5 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery5-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery5").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery5);
+   slideIntervalgallery5 = setInterval(changegallery5,3000);
+      
+      });
+
+var slideIntervalgallery5 = setInterval(changegallery5 ,3000);
+
+
+    function changegallery5() {
+       plusSlidesgallery5(1);
+        
+    }
+
+    //*************6*//////////// */
+    var slideIndexgallery6 = 1;
+showSlidesgallery6(slideIndexgallery6);
+
+
+// Next/previous controls
+function plusSlidesgallery6(n) {
+  showSlidesgallery6(slideIndexgallery6 += n);
+}
+
+// Thumbnail image controls
+function currentSlidegallery6(n) {
+  showSlidesgallery6(slideIndexgallery6 = n);
+}
+
+function showSlidesgallery6(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide-gallery6");
+ 
+  if (n > slides.length) {slideIndexgallery6 = 1}
+  if (n < 1) {slideIndexgallery6 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndexgallery6-1].style.display = "block";
+
+ 
+
+  
+}
+
+document.getElementById("slidegallery6").addEventListener("click", function() {
+  clearInterval(slideIntervalgallery6);
+   slideIntervalgallery6 = setInterval(changegallery6,3000);
+      
+      });
+
+var slideIntervalgallery6 = setInterval(changegallery6 ,3000);
+
+
+    function changegallery6() {
+       plusSlidesgallery6(1);
+        
+    }
