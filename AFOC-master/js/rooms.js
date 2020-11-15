5dc6bff1b773d44bf52366d093a6e94780216aa9
ibti1995 <ibtissembouchnak@gmail.com>
2020-11-15 23:49:20 +0100

    pull

--- AFOC-master/js/rooms.js
+++ AFOC-master/js/rooms.js
@@ -1,5 +1,5 @@
-var slideIndex = [1,1,1,1,1,1,1,1,1];
-var slideId = ["slides1", "slides2","slides3","slides4","slides5","slides6","mySlides7", "mySlides8", "mySlides9"]
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideId = ["slides1", "slides2","slides3","slides4","slides5","slides6","mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15"]
 showSlides6(1, 0);
 showSlides6(1, 1);
 showSlides6(1, 2);
@@ -9,6 +9,13 @@ showSlides6(1, 5);
 showSlides6(1, 6);
 showSlides6(1, 7);
 showSlides6(1, 8);
+showSlides6(1, 9);
+showSlides6(1, 10);
+showSlides6(1, 11);
+showSlides6(1, 12);
+showSlides6(1, 13);
+showSlides6(1, 14);
+
 
 function plusSlides6(n, no) {
     console.log(no , n );
@@ -39,8 +46,8 @@ showSlidesac(1);
     var ji;
     var slidesac = document.getElementsByClassName("mySlides3");
     var dots = document.getElementsByClassName("demo3");
-  
-  
+
+
     for (ji = 0; ji < slidesac.length; ji++) {
       slidesac[ji].style.display = "none";
     }
@@ -49,7 +56,7 @@ showSlidesac(1);
       }
     slidesac[n-1].style.display = "block";
     dots[n-1].className += " active6";
-    
-  
-    
+
+
+
   }
